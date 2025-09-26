67dc89ff76f3c6a1d7bca40d5cb7430beb759d8b
Zainii <ahmed.zain@blssol.com>
2025-09-26 18:52:45 +0500

    fr asof hff l

--- liquidationBot.js
+++ liquidationBot.js
@@ -307,10 +307,11 @@ async function getBorrowerData(address) {
 
 
     if (process.env.MAX_TEST_VAULT_BALANCE) {
-        const cap = BigInt(process.env.MAX_TEST_VAULT_BALANCE);
-        if (VaultBalance > cap) {
-            VaultBalance = cap;
-        }
+        VaultBalance = BigInt(process.env.MAX_TEST_VAULT_BALANCE);
+        // const cap = BigInt(process.env.MAX_TEST_VAULT_BALANCE);
+        // if (VaultBalance > cap) {
+        //     VaultBalance = cap;
+        // }
     }
 
 
